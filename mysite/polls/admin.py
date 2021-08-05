918768d0c246305d67f41b6abeb36e02889c34d8
FrancoGroenewald <franco@softwareworx.co.za>
2021-08-05 18:01:14 +0200

    create polls

---
+++ mysite/polls/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Question
+
+# Register your models here.
+admin.site.register(Question)
+
+#https://docs.djangoproject.com/en/2.2/intro/tutorial03/
